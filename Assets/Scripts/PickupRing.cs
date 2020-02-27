e8e2075f9c1c53c5ba9a86eaf7dabacbeaaca3f3
Lemonoak <45938598+Lemonoak@users.noreply.github.com>
2020-02-27 12:52:11 +0100

    Added basic pickup script
    Added basic pickup script

---
+++ Assets/Scripts/PickupRing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupRing : MonoBehaviour
+{
+    public GameObject ringToHold;
+
+    void Update()
+    {
+        FindRing();
+    }
+
+    void FindRing()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if(hit.collider != null)
+            {
+                ringToHold = hit.transform.gameObject;
+            }
+        }
+        if(Input.GetMouseButton(0) && ringToHold != null)
+        {
+            ringToHold.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, ringToHold.transform.position.z);
+        }
+        if(Input.GetMouseButtonUp(0) && ringToHold != null)
+        {
+            ringToHold = null;
+        }
+    }
+
+}
